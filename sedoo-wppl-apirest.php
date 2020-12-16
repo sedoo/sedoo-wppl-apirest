e1cbbaa02c5a74f22c5829f3a1798f5f2df107db
ngruwe <nicolas.gruwe@obs-mip.fr>
2020-12-16 13:12:20 +0100

    try to get mapped url with custom func

--- sedoo-wppl-apirest.php
+++ sedoo-wppl-apirest.php
@@ -18,6 +18,55 @@
  * Docs : https://developer.wordpress.org/rest-api/extending-the-rest-api/routes-and-endpoints/#creating-endpoints 
  */
 
+function get_domain_mapped_url( $custom_blog_id ) {
+
+    // Enable WordPress DB connection
+    global $wpdb;
+
+    // To reduce the number of database queries, save the results the first time we encounter each blog ID.
+    static $return_url = array();
+
+    $wpdb->dmtable = $wpdb->base_prefix . 'domain_mapping';
+
+    if ( !isset( $return_url[ $custom_blog_id ] ) ) {
+            $s = $wpdb->suppress_errors();
+
+            if ( get_site_option( 'dm_no_primary_domain' ) == 1 ) {
+                    $domain = $wpdb->get_var( "SELECT domain FROM {$wpdb->dmtable} WHERE blog_id = '{$custom_blog_id}' AND domain = '" . $wpdb->escape( $_SERVER[ 'HTTP_HOST' ] ) . "' LIMIT 1" );
+                    if ( null == $domain ) {
+                            $return_url[ $custom_blog_id ] = untrailingslashit( get_site_url( $custom_blog_id ) );
+                            return $return_url[ $custom_blog_id ];
+                    }
+            } else {
+                    // get primary domain, if we don't have one then return original url.
+                    $domain = $wpdb->get_var( "SELECT domain FROM {$wpdb->dmtable} WHERE blog_id = '{$custom_blog_id}' AND active = 1 LIMIT 1" );
+                    if ( null == $domain ) {
+                            $return_url[ $wpdb->blogid ] = untrailingslashit( get_site_url( $custom_blog_id ) );
+                            return $return_url[ $custom_blog_id ];
+                    }
+            }
+
+            $wpdb->suppress_errors( $s );
+            if ( false == isset( $_SERVER[ 'HTTPS' ] ) )
+                    $_SERVER[ 'HTTPS' ] = 'Off';
+            $protocol = ( 'on' == strtolower( $_SERVER[ 'HTTPS' ] ) ) ? 'https://' : 'http://';
+            if ( $domain ) {
+                    $return_url[ $custom_blog_id ] = untrailingslashit( $protocol . $domain  );
+                    $setting = $return_url[ $custom_blog_id ];
+            } else {
+                    $return_url[ $custom_blog_id ] = false;
+            }
+    } elseif ( $return_url[ $custom_blog_id ] !== FALSE) {
+            $setting = $return_url[ $custom_blog_id ];
+    }
+
+    return $setting;
+}
+
+
+
+
+
 /**
  * Get blog sites
  * @return array The list of public sites
@@ -78,7 +127,9 @@ function sedoo_wppl_restapi_get_all_sites_url() {
     $sites_list['sites'] = get_sites();
     foreach($sites_list['sites'] as $site) {
         switch_to_blog( $site->blog_id );
-            $site->site_url = get_site_url();
+            $site->site_url_2 = get_site_url();
+            $site->site_url_3 = get_site_url();
+            $site->site_url = get_domain_mapped_url($site->blog_id);
         restore_current_blog();
     }
 
